69f9e7a65f652c3fa409bc0d7d22a17b2e9a48c1
Aaron S. <132579960+ar73r0@users.noreply.github.com>
2025-04-01 12:00:20 +0200

    rabbitmq in commentaar
    test_monitoring_consumer.py

--- test_monitoring_consumer.py
+++ test_monitoring_consumer.py
@@ -17,19 +17,20 @@ routing_key_monitoring_success = 'monitoring.success'
 routing_key_monitoring_failure = 'monitoring.failure'
 queue_monitoring='monitoring'
 
-
+"""
 channel.exchange_declare(exchange=exchange_monitoring, exchange_type='direct', durable=True)
 channel.queue_declare(queue=queue_monitoring, durable=True)
 channel.queue_bind(exchange=exchange_monitoring, queue=queue_monitoring, routing_key=routing_key_monitoring_success)
 channel.queue_bind(exchange=exchange_monitoring, queue=queue_monitoring, routing_key=routing_key_monitoring_failure)
-
+"""
 
 def monitoring_callback(ch, method, properties, body):
     print(f"[MONITORING] Routing Key: {method.routing_key}")
     print(body.decode())
 
-
+"""
 channel.basic_consume(queue="monitoring", on_message_callback=monitoring_callback, auto_ack=True)
 print("Monitoring... ")
 channel.start_consuming()
 
+"""
