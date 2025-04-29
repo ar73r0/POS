70408140c83bad904b6b2e1f7eca3c97eefcc18a
Lucas <lucas.huygen@student.ehb.be>
2025-04-29 15:09:40 +0200

    verplaatsing module event sync

---
+++ odoo/addons/pos_custom/event_sync/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Event Sync with RabbitMQ',
+    'version': '1.0',
+    'depends': ['event'],
+    'author': 'Attendify',
+    'category': 'Events',
+    'summary': 'Sync events to RabbitMQ on create/update/delete',
+    'description': 'This module sends event.event model updates to RabbitMQ in XML format.',
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
