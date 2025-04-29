c9a86d31c8e1fa65c41e928383064af112bda28a
Lucas <lucas.huygen@student.ehb.be>
2025-04-29 13:59:45 +0200

    setup module voor connectie events en rabbitMQ

---
+++ odoo/addons/pos_custom/customer_rabbit_connector/models/event_sync.py
@@ -0,0 +1,38 @@
+from odoo import models, fields, api
+import logging
+#code module event naar rabbit
+
+_logger = logging.getLogger(__name__)
+
+class EventSync(models.Model):
+    _inherit = 'event.event'  # inheret excisting module odoo
+
+    def _send_event_to_rabbitmq(self, operation):
+        """
+        Sends event data to RabbitMQ.
+        """
+
+        
+        # code hier voor XML formatting etc
+
+
+        _logger.info("Sending event %s to RabbitMQ [%s]", self.id, operation)
+      
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super(EventSync, self).create(vals_list)
+        for record in records:
+            record._send_event_to_rabbitmq('create')
+        return records
+
+    def write(self, vals):
+        res = super(EventSync, self).write(vals)
+        for record in self:
+            record._send_event_to_rabbitmq('update')
+        return res
+
+    def unlink(self):
+        for record in self:
+            record._send_event_to_rabbitmq('delete')
+        return super(EventSync, self).unlink()
